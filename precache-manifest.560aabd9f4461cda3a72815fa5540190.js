f5077a4f2dce6c4ed41d745987b32682b1de8fc7
pepsjain <prachitruejain@gmail.com>
2020-09-28 11:15:20 +0530

    Updates

---
+++ precache-manifest.560aabd9f4461cda3a72815fa5540190.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "20ff6e6ce06ba5b1ff7ef49c7b27f57d",
+    "url": "/ToDo-App/index.html"
+  },
+  {
+    "revision": "b97628d915186aacbee2",
+    "url": "/ToDo-App/static/css/main.6a4dff78.chunk.css"
+  },
+  {
+    "revision": "348dfb36a0099614f4f0",
+    "url": "/ToDo-App/static/js/2.a41d0574.chunk.js"
+  },
+  {
+    "revision": "5f8be6edcb631ea3c834b8459dc2c264",
+    "url": "/ToDo-App/static/js/2.a41d0574.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b97628d915186aacbee2",
+    "url": "/ToDo-App/static/js/main.58df2c8b.chunk.js"
+  },
+  {
+    "revision": "a466f2e91cbf3c2eefc2",
+    "url": "/ToDo-App/static/js/runtime-main.3eeac92e.js"
+  }
+]);
